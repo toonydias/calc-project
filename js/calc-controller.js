020a38b4bce4da7e04127f2b779276bdd66c0a96
toonydias <diasjr.antonio@gmail.com>
2019-02-15 16:23:23 -0200

    Manipulando o DOM

--- js/calc-controller.js
+++ js/calc-controller.js
@@ -1,7 +1,16 @@
 class CalcController {
 	constructor(){
 		this._displayCalc = "0";
-		this._dataAtual;
+		this._currentDate;
+		this.initialize();
+	}
+	initialize (){
+		let displayCalcEl = document.querySelector("#display");
+		let dateEl = document.querySelector("#data");
+		let timeEl = document.querySelector("#hora");
+		displayCalcEl.innerHTML = "4567";
+		dateEl.innerHTML = "01/05/2019";
+		timeEl.innerHTML = "00:00";
 	}
 	get displayCalc(){
 		return this._displayCalc;
@@ -9,10 +18,10 @@ class CalcController {
 	set displayCalc(valor){
 		this._displayCalc = valor;
 	}
-	get dataAtual(){
-		return this._dataAtual;
+	get currentDate(){
+		return this._currentDate;
 	}
-	set dataAtual(data){
-		this._dataAtual = data;
+	set currentDate(data){
+		this._currentDate = data;
 	}
 }
