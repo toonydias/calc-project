7410cb3e008a3da9fea2d785ae39268310f7c01e
toonydias <diasjr.antonio@gmail.com>
2019-02-18 22:46:01 -0300

    Eventos click

--- js/calc-controller.js
+++ js/calc-controller.js
@@ -6,6 +6,7 @@ class CalcController {
 		this._timeEl = document.querySelector("#hora");
 		this._currentDate;
 		this.initialize();
+		this.initButtonsEvents();
 	}
 	initialize (){
 		this.setDisplayDateTime ();
@@ -18,6 +19,15 @@ class CalcController {
 		});
 			this.displayTime = this.currentDate.toLocaleTimeString(this.locale);
 		}, 1000);
+	}
+
+	initButtonsEvents(){
+		let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+		buttons.forEach(btn=>{
+			btn.addEventListener('click', e=>{
+				console.log(btn.className.baseVal.replace("btn-",""));
+			});
+		});
 	}
 	setDisplayDateTime (){
 		this.displayDate = this.currentDate.toLocaleDateString(this.locale);
