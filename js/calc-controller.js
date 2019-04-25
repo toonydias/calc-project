b75c0218d22c643a0b08a0d05cfc8fc517a69c43
toonydias <diasjr.antonio@gmail.com>
2019-04-24 21:06:24 -0300

    Update calc-controller.js

--- js/calc-controller.js
+++ js/calc-controller.js
@@ -10,6 +10,14 @@ class CalcController {
 		this._currentDate;
 		this.initialize();
 		this.initButtonsEvents();
+		this.initKeybord();
+	}
+	copyToClipboard (){
+		let input = document.createElement('input');
+		input.value = this.displayCalc;
+		document.body.appendChild('input');
+		input.select();
+		document.execCommand('Copy');
 	}
 	initialize (){
 		this.setDisplayDateTime ();
@@ -23,6 +31,48 @@ class CalcController {
 			this.displayTime = this.currentDate.toLocaleTimeString(this.locale);
 		}, 1000);
 		this.setLastNumbertoDisplay();
+	}
+	initKeybord(){
+		document.addEventListener('keyup', e=>{
+			switch (e.key){
+			case "Escape":
+				this.clearAll();
+			break;
+			case "Backspace":
+				this.clearEntry();
+			break;
+			case "+":
+			case "-":
+			case "/":
+			case "*":
+			case "%":
+				this.addOperation(e.key);
+			break;
+			case "Enter":
+			case "=":
+				this.calc();
+			break;
+			case ".":
+			case ",":
+				this.addDot('.');
+			break;
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+				this.addOperation(parseInt(e.key));
+				break;
+			case 'c':
+				if (e.ctrlKey) this.copyToClipboard();
+					break;
+		}
+		});
 	}
 	addEventListenerAll(element, events, fn){
 		events.split(' ').forEach(event => {
